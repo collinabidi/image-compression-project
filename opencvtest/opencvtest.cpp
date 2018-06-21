d2f6202709c8d9bd12857ffcc1dfc337015e66f7
Collin Abidi <cba15@pitt.edu>
2018-06-20 17:48:00 -0700

    created multi-file image processing
    currently takes in all files in a directory and runs a greyscale conversion, outputing new_IMAGENAME.TYPE.

--- opencvtest/opencvtest.cpp
+++ opencvtest/opencvtest.cpp
@@ -5,76 +5,62 @@
 
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <windows.h>
+#include <tchar.h>
+#include <string.h>
+#include <stdio.h>
 
 using namespace cv;
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	//Open the default video camera
-	VideoCapture cap(0);
-	int lowThreshold;
-	int const max_lowThreshold = 100;
-	int ratio = 3;
-	int kernel_size = 3;
+	vector<String> fn;
+	String directory = "C:/Desktop/opencvtest/opencvtest/data";
+	glob(directory + "/*.*", fn, false);
+
+	size_t count = fn.size(); // number of images in folder
+
+	vector<Mat> images(fn.size());
 
 
-	// if not success, exit program
-	if (cap.isOpened() == false)
-	{
-		cout << "Cannot open the video camera" << endl;
-		cin.get(); //wait for any key press
-		return -1;
-	}
-
-	double dWidth = cap.get(CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
-	double dHeight = cap.get(CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
-
-	cout << "Resolution of the video : " << dWidth << " x " << dHeight << endl;
-
-	string window_name = "My Camera Feed";
-	namedWindow(window_name); //create a window called "My Camera Feed"
-
-	createTrackbar("Min Threshold", window_name, &lowThreshold, max_lowThreshold);
-
-	while (true)
-	{
-		Mat frame, greyFrame, dst, detected_edges;
-		bool bSuccess = cap.read(frame); // read a new frame from video 
-
-										 //Breaking the while loop if the frames cannot be captured
-		if (bSuccess == false)
+	// loop through images in folder
+		for (size_t i = 0; i < count; i++)
 		{
-			cout << "Video camera is disconnected" << endl;
-			cin.get(); //Wait for any key press
-			break;
+			// makes sure image isn't already converted
+			String name = fn[i].substr(fn[i].find_last_of("\\") + 1, fn[i].length());	// get filename
+			if (name.find("new_"))
+			{
+				// read and display the image
+				images[i] = imread(fn[i]);
+				imshow((String)fn[i], images[i]);
+				cout << fn[i] << endl;
+
+				// convert image to greyscale
+				cvtColor(images[i], images[i], CV_BGR2GRAY);
+
+				// save image to new file
+				cout << name << endl;
+				imwrite(directory + "/new_" + name, images[i]);
+				cout << "Writing to " + directory + "/new_" + name << endl;
+
+				// blur image
+				//blur(grey, detected_edges, Size(3, 3));
+
+				// apply Canny transform
+				//Canny(detected_edges, detected_edges, 50, 50 * 3, 3);
+
+				// create black image and copy image result to dst and detected_edges
+				//dst = Scalar::all(0);
+				//image.copyTo(dst, detected_edges);
+
+				// display result
+				//cout << i << endl;
+				//String yes = fn[i];
+				//namedWindow(yes);
+				//imshow(yes, dst);
+			}
 		}
-		//convert image to greyscale
-		cvtColor(frame, greyFrame, CV_BGR2GRAY);
-
-		/// Reduce noise with a kernel 3x3
-		blur(greyFrame, detected_edges, Size(3,3));
-
-		/// Canny detector
-		Canny(detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size);
-
-		/// Using Canny's output as a mask, we display our result
-		dst = Scalar::all(0);
-
-		frame.copyTo(dst, detected_edges);
-
-		//show the frame in the created window
-		imshow(window_name, dst);
-
-		//wait for for 10 ms until any key is pressed.  
-		//If the 'Esc' key is pressed, break the while loop.
-		if (waitKey(10) == 27)
-		{
-			cout << "Esc key is pressed by user. Stoppig the video" << endl;
-			break;
-		}
+		waitKey(0);
+		return 0;
 	}
-
-	return 0;
-
-}
